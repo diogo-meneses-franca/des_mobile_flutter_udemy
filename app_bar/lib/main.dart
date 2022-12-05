ba43095d052caa5aaf3a8cddbd9422d127761d2f
Diogo Meneses França <diogo.meneses.franca@gmail.com>
2022-12-05 18:29:47 -0300

    completed the sound bar exercise

--- app_bar/lib/main.dart
+++ app_bar/lib/main.dart
@@ -7,44 +7,44 @@ void main() {
 
 class AppBar extends StatelessWidget {
   const AppBar({Key? key}) : super(key: key);
+
   void playSound(int soundNumber) {
     final audioPlayer = AudioCache();
     audioPlayer.play('note$soundNumber.wav');
+  }
+
+  Expanded mySoundBar({required int numberOfSound, required Color colorName}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: colorName,
+        ),
+        onPressed: () {
+          playSound(numberOfSound);
+        },
+        child: Text(""),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-                child: TextButton(
-                style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue,
-                padding: const EdgeInsets.all(16.0),
-                textStyle: const TextStyle(fontSize: 40),
-              ),
-              onPressed: () {
-                playSound(1);
-              },
-              child: Text("Play"),
-            )),
-            Expanded(
-                child: TextButton(
-                  style: TextButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: Colors.red,
-                    padding: const EdgeInsets.all(16.0),
-                    textStyle: const TextStyle(fontSize: 40),
-                  ),
-                  onPressed: () {
-                    playSound(2);
-                  },
-                  child: Text("Play"),
-                ))
-          ],
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              mySoundBar(numberOfSound: 1, colorName: Colors.red),
+              mySoundBar(numberOfSound: 2, colorName: Colors.orange),
+              mySoundBar(numberOfSound: 3, colorName: Colors.yellow),
+              mySoundBar(numberOfSound: 4, colorName: Colors.green),
+              mySoundBar(numberOfSound: 5, colorName: Colors.teal),
+              mySoundBar(numberOfSound: 6, colorName: Colors.blue),
+              mySoundBar(numberOfSound: 7, colorName: Colors.purple)
+            ],
+          ),
         ),
       ),
     );
