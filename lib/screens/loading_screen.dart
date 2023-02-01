3729b425b1f9c48e4118e16a3ef6e2a59af64e3b
diogo-meneses-franca <diogo.meneses.franca@gmail.com>
2023-02-01 17:50:02 -0300

    fixed bug with http get method

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'dart:math';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
@@ -25,12 +27,26 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getData() async {
+    String latitute = '37.785834';
+    String longitude = '-122.406417';
+    String apiKey = '02c53786f7dd12355b6b5ed72d04bf18';
     var url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat={37.785834}&lon={-122.406417}&appid={b7df78a41ae6f3bef25d092171c4edbb}');
-    var response = await http.get(url);
+        'https://api.openweathermap.org/data/2.5/weather?lat=$latitute&lon=$longitude&appid=$apiKey');
+    http.Response response = await http.get(url);
     if (response.statusCode == 200) {
       String data = response.body;
-      print(data);
+      double temperature = jsonDecode(data)['main']['temp'];
+      int condition = jsonDecode(data)['weather'][0]['id'];
+      String cityName = jsonDecode(data)['name'];
+
+      double celciusTemperature = temperature - 273.15;
+
+      double roundedTemperature =
+          double.parse(celciusTemperature.toStringAsFixed(2));
+
+      print("Temperatura: $roundedTemperature");
+      print("Condition: $condition");
+      print("City Name: $cityName");
     } else {
       print(response.statusCode);
     }
@@ -38,6 +54,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return const Scaffold();
   }
 }
